329d2172ca55ae702e1f6b2e777dd500693f1534
omeryildizce <omeryildizce@gmail.com>
2022-07-11 17:12:11 +0300

    Django projesi
    İlk django projesi bitirildi.

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("books", views.books, name="books"),
+    path("authors", views.authors, name="authors"),
+    path("authordetails/<int:authorId>", views.autorDetails, name="authorDetails"),
 ]
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
-
+from django.http import Http404, HttpResponse 
+from django.template import loader
+from .models import Author
 # Create your views here.
 
 def index(request):
     return HttpResponse("Anasayfa")
+
+def authors(request):
+    # template = loader.get_template('authors.html')
+    context = {
+        "authors_list" : Author.objects.all()
+    }
+    # return HttpResponse(template.render(context, request))
+    # alternatif
+    return render(request, "authors.html", context)
+def books(request):
+    return HttpResponse("Kitaplar")
+
+def autorDetails(request, authorId):
+    try:
+        context = {
+            "author_detail" : Author.objects.get(pk=authorId)
+        } 
+    except Author.DoesNotExist:
+        raise Http404("Yazar bulunamadı")
+    return render(request, "authorDetail.html", context)
+
